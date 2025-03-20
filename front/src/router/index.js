6f702ea414aef620624515709ba07cefc630e2c7
Fiorella <fioaricol@alu.edu.gva.es>
2025-03-20 15:03:46 +0100

    Starting the welcome view
    Starting a welcome view , there are some problems tho.

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -5,10 +5,17 @@ import RegisterView from '../views/RegisterView.vue'
 import VerifyEmailView from '../views/VerifyEmailView.vue'
 import CambiarPasswordView from '../views/CambiarPasswordView.vue'
 import PanelControlView from '../views/PanelControlView.vue'
+import WelcomeView from '../views/WelcomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: WelcomeView,
+      meta: { requiresAuth: false }
+    },
     {
       path: '/',
       name: 'home',
