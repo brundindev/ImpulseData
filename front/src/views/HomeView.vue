88fd75a44d7dbbbfe7ae01033e93ac45b12ff7fd
Fiorella <fioaricol@alu.edu.gva.es>
2025-05-09 10:39:43 +0200

    Implementing CompaniesList

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -69,42 +69,13 @@
         
         <!-- Lista de empresas -->
         <div v-else class="forms-section">
-          <h2>Mis empresas</h2>
-          
-          <div v-if="empresas.length === 0" class="empty-state">
-            <p>No tienes empresas registradas todavía.</p>
-            <button class="btn btn-secondary" @click="showFormModal = true">Crear</button>
-          </div>
-          
-          <div v-else class="forms-list">
-            <div v-for="empresa in empresas" :key="empresa.id" class="form-card">
-              <div class="form-info" @click="verEmpresa(empresa)">
-                <h3>{{ empresa.nombre }}</h3>
-                <p>{{ empresa.descripcion }}</p>
-                <div class="form-meta">
-                  <span>Ciudad: {{ empresa.ciudad }}</span>
-                  <span>Creada: {{ formatDate(empresa.fechaCreacion) }}</span>
-                </div>
-                <div class="form-stats">
-                  <span class="stat-pill">{{ empresa.numDepartamentos }} departamentos</span>
-                  <span class="stat-pill">{{ empresa.numCentros }} centros</span>
-                  <span class="stat-pill">{{ empresa.numFormaciones }} formaciones</span>
-                </div>
-              </div>
-              <div class="form-actions">
-                <button class="button" @click="editarEmpresa(empresa)">Editar</button>
-                <button class="button" @click="verEmpresa(empresa)">Ver</button>
-                <button class="noselect" @click="confirmarEliminar(empresa)">
-                  <span class="text">Eliminar</span>
-                  <span class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                      <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
-                    </svg>
-                  </span>
-                </button>
-              </div>
-            </div>
-          </div>
+          <CompaniesList 
+            :empresas="empresas"
+            @create-company="() => { showFormModal = true; modoEdicion = false; }"
+            @edit-company="(empresa) => { editarEmpresa(empresa); showFormModal = true; }"
+            @view-company="(empresa) => { verEmpresa(empresa); showViewModal = true; }"
+            @delete-company="confirmarEliminar"
+          />
         </div>
       </div>
       
@@ -707,6 +678,7 @@ import AnimatedNumber from '../components/AnimatedNumber.vue';
 import ScrollAnimation from '../components/ScrollAnimation.vue';
 import AlicanteBanner from '../components/home/AlicanteBanner.vue';
 import StatsPanel from '../components/home/StatsPanel.vue';
+import CompaniesList from '../components/home/CompaniesList.vue';
 import AppFooter from '../components/home/AppFooter.vue';
 import GeminiButton from '../components/GeminiButton.vue';
 // Importar PDFService
